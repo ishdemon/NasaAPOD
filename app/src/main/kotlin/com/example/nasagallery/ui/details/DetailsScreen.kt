d13493421fd6bb21ef4ec52c7e8ec10064c23bb3
ishdemon <ishdemon10@gmail.com>
2022-09-30 01:06:57 +0530

    Added zoom feature in detail Screen

--- app/src/main/kotlin/com/example/nasagallery/ui/details/DetailsScreen.kt
+++ app/src/main/kotlin/com/example/nasagallery/ui/details/DetailsScreen.kt
@@ -13,16 +13,11 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import coil.annotation.ExperimentalCoilApi
 import com.example.nasagallery.common.DataState.Success
 import com.example.nasagallery.ui.components.FieldState
 import com.example.nasagallery.ui.components.PagerIndicator
